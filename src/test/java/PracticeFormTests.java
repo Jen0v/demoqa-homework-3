cb8751f84fac129124484b843bfef9f39884a6b3
Anna <jenova.anya@yandex.ru>
2024-09-12 00:47:36 +0300

    added .PracticeFormTests

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -8,43 +8,54 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTests {
+    //Настройки
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
 
     }
 
     @Test
     void fillFromTest() {
+        //Открытие формы
         open("/automation-practice-form");
+
+        //Имя, фамилия
         $("#firstName").setValue("Anna");
         $("#lastName").setValue("Jen");
+
+        //Почта
         $("#userEmail").setValue("anna@googl.com");
+
+        //Пол
         $("#genterWrapper").$(byText("Female")).click();
+
+        //Номер телефона (10 цифр)
         $("#userNumber").setValue("9000000001");
 
-
+        //День рождения
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").sendKeys("1997");
         $(".react-datepicker__month-select").sendKeys("October");
         $(".react-datepicker__day--015").click();
         $("#subjectsContainer input").click();
 
-
+        //Темы, хобби
         $("#subjectsContainer input").setValue("History").pressEnter();
-       $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
 
-
+        //Картинка
         $("#uploadPicture").uploadFromClasspath("photo.jpg");
 
-
+        //Текущий адрес
         $("#currentAddress").setValue("Some street 1");
 
 
+        //Штат, город
         $("#state input").sendKeys("Haryana");
         $("#state input").pressEnter();
         $("#city input").sendKeys("Karnal");
@@ -52,8 +63,7 @@ public class PracticeFormTests {
         $("#submit").click();
 
 
-        $("#submit").click();
-
+        //Проверка
         $(".modal-content").shouldHave (text("Thanks for submitting the form"));
         $(".modal-content").shouldHave (text("Anna Jen"));
         $(".modal-content").shouldHave (text("anna@googl.com"));
